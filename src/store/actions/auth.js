7bf4f9516eaf9e66855728f5bafe8b1a992b16a5
BertelsBenjamin <bertels123@gmail.com>
2020-11-09 16:51:39 +0100

    auth action setup

---
+++ src/store/actions/auth.js
@@ -0,0 +1,61 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const API_KEY = "AIzaSyAjwM0I4i4WXXo0etpDez3Msz9PPOPvmQ8";
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+
+    axios
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        authData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        dispatch(authFail(error));
+      });
+  };
+};
